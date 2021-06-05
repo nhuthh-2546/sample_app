b59bc8c4a543099054f8d1ec4d3e5c15a553a9ce
Ha Huu Nhut <ha.huu.nhut@sun-asterisk.com>
2021-06-05 17:14:47 +0700

    Chapter 7: sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,27 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find_by(id: params[:id])
+    return if @user
+    render :file => "#{Rails.root}/public/404.html",  layout: false, status: :not_found
+  end
+  
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = t "flash.signup_success"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
