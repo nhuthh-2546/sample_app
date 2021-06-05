d919237c5d26f10cfa457fbdec215baf332086ef
Ha Huu Nhut <ha.huu.nhut@sun-asterisk.com>
2021-06-05 17:14:47 +0700

    Chapter 5: Filling in the layout

--- test/helpers/application_helper_test.rb
+++
@@ -1,7 +0,0 @@
-# require 'test_helper'
-# class ApplicationHelperTest < ActionView::TestCase
-#   test "full title helper" do
-#     assert_equal full_title, 
-#     assert_equal full_title("Help"), 
-#   end
-# end
